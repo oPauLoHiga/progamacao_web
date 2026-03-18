86d1acd5ddec9a5e44e33645982428647d410347
Paulo Higa <paulojhiga@gmail.com>
2026-03-18 15:47:12 -0300

    origem
    iniciando

---
+++ Aula1/hellouworld.js
@@ -0,0 +1,9 @@
+console.log('Hello World')
+
+var nome = prompt('Digite seu nome:');
+
+var idade = prompt('Digite sua idade:');
+
+var idadeFutura = parseInt(idade) + 10;
+
+console.log(`${nome}, em 10 anos sua idade será ${idadeFutura} anos`);
